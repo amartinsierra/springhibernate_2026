d327c13a6c662d0d5ccee4c17b95c1225e50a04c
profesor <ajms66@gmail.com>
2026-02-26 20:57:32 +0100

    proyectos 1 al 3

--- 02_prototipo_buscador_thymeleaf/src/main/java/controller/ItemsController.java
+++ 02_prototipo_buscador_thymeleaf/src/main/java/controller/ItemsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import model.Item;
@@ -24,8 +25,37 @@ public class ItemsController {
 		return "resultados";
 		
 	}
-	@GetMapping("/")
-	public String main() {
+	
+	/*@GetMapping("inicio")
+	public String volver() {
+		return "inicio";
+	}*/
+	@PostMapping("alta")
+	public String alta(Model model, 
+			@RequestParam("url") String url,
+			@RequestParam("tematica") String tematica,
+			@RequestParam("descripcion") String descripcion) {
+		Item item=new Item(url,tematica,descripcion);
+		if(itemsService.nuevoItem(item)) {
+			model.addAttribute("mensaje", "Item añadido correctamente!");
+		}else {
+			model.addAttribute("mensaje", "No añadido, URL duplicada!!!");
+		}
+		return "mensaje";
+	}
+	@GetMapping("inicio")
+	public String inicio() {
 		return "inicio";
 	}
+	@GetMapping("nuevo")
+	public String nuevo() {
+		return "nuevo";
+	}
+	@GetMapping({"/","menu"})
+	public String menu() {
+		return "menu";
+	}
+	
+	
+	
 }
