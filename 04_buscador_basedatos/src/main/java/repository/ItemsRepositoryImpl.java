add3ad54e5bffa2ff4606e38813034a02d417c70
profesor <ajms66@gmail.com>
2026-03-02 16:39:07 +0100

    ejercicios 1 al 4

--- 04_buscador_basedatos/src/main/java/repository/ItemsRepositoryImpl.java
+++ 04_buscador_basedatos/src/main/java/repository/ItemsRepositoryImpl.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import model.Item;
 @Repository
 public class ItemsRepositoryImpl implements ItemsRepository {
@@ -21,14 +22,20 @@ public class ItemsRepositoryImpl implements ItemsRepository {
 
 	@Override
 	public List<Item> findByTematica(String tematica) {
-		// pendiente...
-		return null;
+		String jpql="select i from Item i where i.tematica=?1";
+		TypedQuery<Item> query=eManager.createQuery(jpql, Item.class);
+		query.setParameter(1, tematica);
+		return query.getResultList();
 	}
 
 	@Override
 	public Item findFirstByUrl(String url) {
 		// pendiente....
-		return null;
+		String jpql="select i from Item i where i.url=?1";
+		TypedQuery<Item> query=eManager.createQuery(jpql, Item.class);
+		query.setParameter(1, url);
+		List<Item> resultado=query.getResultList();
+		return resultado.size()>0?resultado.get(0):null;
 	}
 
 }
