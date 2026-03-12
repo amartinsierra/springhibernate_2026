e93f55bb687ef7a7f86fd125e4841ff28171a2e3
profesor <ajms66@gmail.com>
2026-03-12 16:52:46 +0100

    proyecto 24

--- 24_microservicio_formacion/src/main/java/init/controller/EstudiantesController.java
+++ 24_microservicio_formacion/src/main/java/init/controller/EstudiantesController.java
@@ -1,14 +1,40 @@
 package init.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import init.dtos.EstudianteDto;
 import init.service.EstudiantesService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 public class EstudiantesController {
 	@Autowired
 	EstudiantesService estudiantesService;
+	@GetMapping("estudiantes")
+	//En spring boot 4, si nos nombres de parámetros coinciden con las varaibles, no es necesario
+	//indicar @RequestParam
+	public ResponseEntity<List<EstudianteDto>> estudiantesCalificacion(double min, double max){
+		return ResponseEntity.ok(estudiantesService.estudiantesRangoCalificaciones(min, max));
+	}
 	
-	
+	@ApiResponses({
+		@ApiResponse(responseCode="201",description = "El estudiante se ha añadido correctamente"),
+		@ApiResponse(responseCode="409",description = "No se ha creado el estudiante, porque no se puede repetir la misma combinación nombre curso")
+	})
+	@PostMapping("estudiantes")
+	public ResponseEntity<Void> altaEstudiante(@RequestBody EstudianteDto estudiante){
+		if(estudiantesService.altaEstudiante(estudiante)) {
+			return ResponseEntity.status(HttpStatus.CREATED).build();
+		}
+		return ResponseEntity.status(HttpStatus.CONFLICT).build();
+	}
 }
--- 24_microservicio_formacion/src/main/java/init/service/EstudiantesSrviceImpl.java
+++ 24_microservicio_formacion/src/main/java/init/service/EstudiantesSrviceImpl.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 
 import init.dtos.EstudianteDto;
@@ -27,13 +30,26 @@ public class EstudiantesSrviceImpl implements EstudiantesService {
 				.retrieve()
 				.body(Alumno[].class))  //Stream<Alumno>
 				.map(a->mapeador.alumnoToEstudiante(a))//Stream<EstudianteDto>
+				.filter(e->e.getCalificacion()>=min&&e.getCalificacion()<=max)
 				.toList();
 	}
 
 	@Override
 	public boolean altaEstudiante(EstudianteDto estudiante) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			restClient.post()
+			.uri(urlBase+"alumnos")
+			.contentType(MediaType.APPLICATION_JSON)
+			.body(mapeador.estudianteToAlumno(estudiante))
+			.retrieve()
+			.toBodilessEntity();
+			return true;
+		}catch(HttpClientErrorException ex) {
+			if(ex.getStatusCode()==HttpStatus.CONFLICT) {
+				return false;
+			}
+			throw ex;
+		}
 	}
 
 }
